745aefcd26e570028decb2b30485e16aefb2465d
OmarSamir <omarsamiir@outlook.com>
2022-06-21 23:50:40 +0200

    Update

---
+++ learn/src/omar/Main.java
@@ -0,0 +1,8 @@
+package src.omar;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(1 + 2);
+        SayHi.Sayhi();
+    }
+}
---
+++ learn/src/omar/SayHi.java
@@ -0,0 +1,12 @@
+package src.omar;
+
+public class SayHi {
+    public static void main(String[] args) {
+        Sayhi();
+    }
+
+    public static void Sayhi() {
+        System.out.println("Hello");
+
+    }
+}
